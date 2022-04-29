099f2a3e02319ec774844e392f73b987f5beb028
Adrian <adrianhenry2115@gmail.com>
2022-04-29 11:56:39 -0400

    mern module algo

--- 13-character-count/Solved/character-count.js
+++ 13-character-count/Solved/character-count.js
@@ -1,6 +1,6 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {
+var characterCount = function (str) {
   var charMap = {};
 
   for (var i = 0; i < str.length; i++) {
--- 14-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ 14-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,7 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {};
+let arr = [1, 2, 3, 4, 5]
+
+var productOfLargestTwo = function (arr) {
+
+}
